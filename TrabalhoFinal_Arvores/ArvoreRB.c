40b800c663558721a5dcc747fc2cdb93a2f0a2e4
Joyce Mayara <joycemayara.al@gmail.com>
2017-06-16 18:14:35 -0300

    [ADD] Função inicializar árvore RB.

--- TrabalhoFinal_Arvores/ArvoreRB.c
+++ TrabalhoFinal_Arvores/ArvoreRB.c
@@ -2,13 +2,28 @@
 #include <stdio.h>
 #include "ArvoreRB.h"
 
-void insereArvoreRB(){
-    printf("o");
-}
-void removeArvoreRB(){
-    printf("o");
-}
-void converteRBto234(){
-    printf("o");
+rb* rb_inicializaArvore() {
+	rb* sentinela; 
+	sentinela = (rb*) calloc(1, sizeof(rb)); 	
+	sentinela->info = 1000; 
+	sentinela->cor = 'p'; 		// O nó sentinela é preto. 
+	sentinela->esq = NULL;
+	sentinela->dir = NULL;
+	sentinela->pai = NULL; 
+
+	return sentinela; 
 }
 
+void rb_insereNo(rb* Arv, int valorNo);
+
+void rb_rotacaoEsq(rb* Arv, rb* noDesbalanceado);
+
+void rb_rotacaoDir(rb* Arv, rb* noDesbalanceado);
+
+void rb_percorreOrdem(rb* Arv);
+
+void rb_balanceamentoInsercao(rb* Arv, rb* noInserido);
+
+void rb_removeNo(rb* Arv, int valor);
+
+void rb_balanceamentoRemocao(rb* Arv, rb* noPai, rb* noSubstituto, int valorRemovido);
