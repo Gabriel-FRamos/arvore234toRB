62479d62c377cbd5696d6371486f3fb7ab6f7de5
Joyce Mayara <joycemayara.al@gmail.com>
2017-06-17 15:14:02 -0300

    [ADD]Operações de rotação à esquerda e a direita.

--- TrabalhoFinal_Arvores/ArvoreRB.c
+++ TrabalhoFinal_Arvores/ArvoreRB.c
@@ -51,9 +51,71 @@ void rb_insereNo(rb* A, int valorNo){ // Todo nó inserido chama a função de b
 
     return; 
 }
-void rb_rotacaoEsq(rb* Arv, rb* noDesbalanceado);
+void rb_rotacaoEsq(rb* Arv, rb* noDesbalanceado) {
+	// Declaração de variáveis auxiliares para auxílio na alteração dos ponteiros pertinentes à rotação
+	rb* filhoDir = noDesbalanceado->dir;		// Filho à dir. do nó desbalanceado 
+	rb* netoEsq = filhoDir->esq;				// Neto à esquerda da sad do nó desbalanceado
+	rb* avo = noDesbalanceado->pai; 
 
-void rb_rotacaoDir(rb* Arv, rb* noDesbalanceado);
+	// Realização da rotação
+	noDesbalanceado->dir = netoEsq;			// Neto à esquerda se torna filho à direita do nó desbalanceado. Se neto à esq for NULL (não existir), não é preciso tratar.  
+
+	if(netoEsq) { 	// Tratamento caso o neto à esq não seja nulo
+		netoEsq->pai = noDesbalanceado; 	// Atualiza-se o campo pai do ex-neto à esquerda
+	}
+
+	filhoDir->esq = noDesbalanceado; 		// Pai/Nó desbalanceado vira filho à esq (do seu filho a dir). A partir daqui, filhoDir não tem mais ligação com neto à esq. 
+	filhoDir->pai = avo; 					// O filho à dir. do nó desbalanceado vira a nova raiz da subárvore: Atualização do pai do filho à dir
+	noDesbalanceado->pai = filhoDir; 		// Atualiza o pai da antiga raiz: filho a direita vira pai da antiga raiz/nó desbalanceado
+	
+	// 'filhoDir->pai' agora aponta (ou tem o mesmo endereço que) o nó 'avo'
+	
+	/*  Antes da rotação, não é possível saber se o nó desbalanceado era filho à dir ou esq do seu pai. 
+	Como após a rotação a posição do nó desbalanceado muda, é necessário atualizar um dos ponteiros de filho do seu pai original, fazendo-o apontar para o nó filho adequado/atualizado após a rotação. 
+	Isso é feito verificando-se o princípio de ordenação da árvore Rubro Negra (que também é uma árvore binária de busca).  
+	*/
+	if(avo->info <= filhoDir->info) {		// Á area que foi balanceada está na sad do pai inicial/original do nó desbalanceado
+	    filhoDir->pai->dir = filhoDir;
+	} else {
+	    filhoDir->pai->esq = filhoDir; 		// Á area que foi balanceada está na sae do pai inicial/original do nó desbalanceado
+	}
+
+	// Ao contrário do que ocorre na Árvore AVL, não é necessário alterar o balanceamento de nós na rotação.
+	return; 
+} // Fim da função 'rb_rotacaoEsq(rb* Arv, rb* noDesbalanceado)'
+
+void rb_rotacaoDir(rb* Arv, rb* noDesbalanceado) {
+	// Declaração de variáveis auxiliares para auxílio na alteração dos ponteiros pertinentes à rotação
+	rb* filhoEsq = noDesbalanceado->esq;		// Filho à esq. do nó desbalanceado 
+	rb* netoDir = filhoEsq->dir;				// Neto à direita da sae do nó desbalanceado
+	rb* avo = noDesbalanceado->pai; 
+
+	// Realização da rotação
+	noDesbalanceado->esq = netoDir;			// Neto à direita se torna filho à esquerda do nó desbalanceado. Se neto à dir for NULL (não existir), não é preciso tratar.  
+
+	if(netoDir) { 	// Tratamento caso o neto à dir não seja nulo
+		netoDir->pai = noDesbalanceado; 	// Atualiza-se o campo pai do ex-neto à direita
+	}
+
+	filhoEsq->dir = noDesbalanceado; 		// Pai/Nó desbalanceado vira filho à dir (do seu filho a esq). A partir daqui, filhoEsq não tem mais ligação com neto à dir. 
+	filhoEsq->pai = avo; 					// O filho à esq. do nó desbalanceado vira a nova raiz da subárvore: Atualização do pai do filho à esq
+	noDesbalanceado->pai = filhoEsq; 		// Atualiza o pai do nó desbalanceado: filho a direita, agora raiz da subárvore corrente, vira pai do nó desbalanceado
+	
+	// 'filhoEsq->pai' agora aponta (ou tem o mesmo endereço que) o nó 'avo'
+	
+	/*  Antes da rotação, não é possível saber se o nó desbalanceado era filho à dir ou esq do seu pai. 
+	Como após a rotação a posição do nó desbalanceado muda, é necessário atualizar um dos ponteiros de filho do avô (pai original do nó desbalanceado), fazendo-o apontar para o nó filho adequado/atualizado após a rotação. 
+	Isso é feito verificando-se o princípio de ordenação da árvore Rubro Negra (que também é uma árvore binária de busca).  
+	*/
+	if(avo->info <= filhoEsq->info) {		// Á area que foi balanceada está na sad do pai inicial/original do nó desbalanceado
+	    filhoEsq->pai->dir = filhoEsq;		// FilhoEsq está a direita do 'avô'
+	} else {								// FilhoEsq está à esquerda do avô (pois os valores de FilhoEsq são menores que a raiz/avô )
+	    filhoEsq->pai->esq = filhoEsq; 		// Á area que foi balanceada está na sae do pai inicial/original do nó desbalanceado
+	}
+
+	// Ao contrário do que ocorre na Árvore AVL, não é necessário alterar o balanceamento de nós na rotação.
+	return; 
+}
 
 void rb_percorreOrdem(rb* Arv);
 
